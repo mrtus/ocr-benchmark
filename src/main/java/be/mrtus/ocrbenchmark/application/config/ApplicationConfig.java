99ba2e90a47ddbfc4ea837fb88611d482dee222d
Michiel Devriese <michieldevriese@gmail.com>
2017-07-13 20:20:11 +0200

    FileLoaderConfig not needed in ApplicationConfig

--- src/main/java/be/mrtus/ocrbenchmark/application/config/ApplicationConfig.java
+++ src/main/java/be/mrtus/ocrbenchmark/application/config/ApplicationConfig.java
@@ -1,17 +1,12 @@
 package be.mrtus.ocrbenchmark.application.config;
 
-import be.mrtus.ocrbenchmark.application.config.properties.FileLoaderConfig;
 import be.mrtus.ocrbenchmark.domain.Benchmark;
 import be.mrtus.ocrbenchmark.domain.FileLoader;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-
-	@Autowired
-	private FileLoaderConfig fileLoaderConfig;
 
 	@Bean
 	public Benchmark benchmark() {
