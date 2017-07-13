aa973a80d61fde3a01cc8920fa46fe6e350b5190
Michiel Devriese <michieldevriese@gmail.com>
2017-07-13 12:27:00 +0200

    Total benchmark time calculation

--- src/main/java/be/mrtus/ocrbenchmark/domain/Benchmark.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/Benchmark.java
@@ -1,6 +1,7 @@
 package be.mrtus.ocrbenchmark.domain;
 
 import be.mrtus.ocrbenchmark.application.config.properties.BenchmarkConfig;
+import java.time.Duration;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,11 +9,56 @@ public class Benchmark extends Thread {
 
 	@Autowired
 	private BenchmarkConfig config;
-	@Autowired
+	private long endTime;
 	private final Logger logger = Logger.getLogger(Benchmark.class.getName());
+	private long startTime;
+
+	public String durationToString(long time) {
+		Duration duration = Duration.ofMillis(time);
+
+		long hours = duration.toHours();
+
+		Duration minutesDuration = duration.minusHours(hours);
+		long minutes = minutesDuration.toMinutes();
+
+		Duration secondsDuration = minutesDuration.minusMinutes(minutes);
+		long seconds = secondsDuration.getSeconds();
+
+		Duration milliesDuratoin = secondsDuration.minusSeconds(seconds);
+		long millies = milliesDuratoin.toMillis();
+
+		String durationString = hours + "h "
+								+ minutes + "m "
+								+ seconds + "." + millies + "s ";
+
+		return durationString;
+	}
 
 	@Override
 	public void run() {
+		this.prepareBenchmark();
 
+		this.startBenchmark();
+
+		this.endBenchmark();
+	}
+
+	private void endBenchmark() {
+		this.endTime = System.currentTimeMillis();
+
+		String duration = this.durationToString(this.endTime - this.startTime);
+
+		this.logger.info("Benchmark ended after " + duration);
+		this.logger.info("====================");
+	}
+
+	private void prepareBenchmark() {
+
+	}
+
+	private void startBenchmark() {
+		this.logger.info("====================");
+		this.logger.info("Benchmark started");
+		this.startTime = System.currentTimeMillis();
 	}
 }
