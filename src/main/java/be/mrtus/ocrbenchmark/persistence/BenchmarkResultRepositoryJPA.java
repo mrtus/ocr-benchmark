0dd30c88d71d96a7f745368f4ac9c6e80a5bc480
Michiel Devriese <michieldevriese@gmail.com>
2017-07-14 00:46:33 +0200

    Persistence call should be transactional

--- src/main/java/be/mrtus/ocrbenchmark/persistence/BenchmarkResultRepositoryJPA.java
+++ src/main/java/be/mrtus/ocrbenchmark/persistence/BenchmarkResultRepositoryJPA.java
@@ -2,6 +2,7 @@ package be.mrtus.ocrbenchmark.persistence;
 
 import be.mrtus.ocrbenchmark.domain.entities.BenchmarkResult;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class BenchmarkResultRepositoryJPA implements BenchmarkResultRepository {
@@ -9,6 +10,7 @@ public class BenchmarkResultRepositoryJPA implements BenchmarkResultRepository {
 	@Autowired
 	private EntityManager entityManager;
 
+	@Transactional
 	@Override
 	public void save(BenchmarkResult result) {
 		this.entityManager.merge(result);
