181fedf72e24e223df5ae523ff3622b5d54b4f7e
Michiel Devriese <michieldevriese@gmail.com>
2017-07-13 20:22:40 +0200

    Setup benchmark file processing

--- src/main/java/be/mrtus/ocrbenchmark/domain/Benchmark.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/Benchmark.java
@@ -1,7 +1,10 @@
 package be.mrtus.ocrbenchmark.domain;
 
 import be.mrtus.ocrbenchmark.application.config.properties.BenchmarkConfig;
+import be.mrtus.ocrbenchmark.domain.entities.LoadedFile;
 import java.time.Duration;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,10 +13,10 @@ public class Benchmark extends Thread {
 	@Autowired
 	private BenchmarkConfig config;
 	private long endTime;
-	private final Logger logger = Logger.getLogger(Benchmark.class.getName());
-	private long startTime;
 	@Autowired
 	private FileLoader fileLoader;
+	private final Logger logger = Logger.getLogger(Benchmark.class.getName());
+	private long startTime;
 
 	public String durationToString(long time) {
 		Duration duration = Duration.ofMillis(time);
@@ -42,7 +45,35 @@ public class Benchmark extends Thread {
 
 		this.startBenchmark();
 
+		try {
+			this.doBenchmark();
+		} catch(InterruptedException ex) {
+			this.logger.log(Level.SEVERE, null, ex);
+		}
+
 		this.endBenchmark();
+
+		System.exit(0);
+	}
+
+	private void doBenchmark() throws InterruptedException {
+		ArrayBlockingQueue<LoadedFile> queue = this.fileLoader.getQueue();
+
+		while(true) {
+			while(queue.peek() == null) {
+				if(!this.fileLoader.isLoading()) {
+					break;
+				}
+			}
+
+			LoadedFile lf = queue.poll();
+
+			if(lf == null) {
+				break;
+			}
+
+			this.processFile(lf);
+		}
 	}
 
 	private void endBenchmark() {
@@ -55,12 +86,27 @@ public class Benchmark extends Thread {
 	}
 
 	private void prepareBenchmark() {
+		this.logger.info("====================");
+		this.logger.info("Preparing benchmark");
+
 		this.fileLoader.start();
+
+		try {
+			this.logger.info("Sleeping 5000 ms");
+			Thread.sleep(5000);
+		} catch(InterruptedException ex) {
+			Logger.getLogger(Benchmark.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	private void processFile(LoadedFile lf) {
+		this.logger.info("Processing file " + lf.getPath().toString());
+		
 	}
 
 	private void startBenchmark() {
-		this.logger.info("====================");
-		this.logger.info("Benchmark started");
+		this.logger.info("Starting benchmark");
+
 		this.startTime = System.currentTimeMillis();
 	}
 }
