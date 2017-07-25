416b1605c2c99e07db6ce4608def4f0e07b8d16d
Michiel Devriese <michieldevriese@gmail.com>
2017-07-25 13:56:09 +0200

    Work with interface

--- src/main/java/be/mrtus/ocrbenchmark/domain/libraries/TesseractLibrary.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/libraries/TesseractLibrary.java
@@ -4,13 +4,14 @@ import be.mrtus.ocrbenchmark.domain.entities.LoadedFile;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 public class TesseractLibrary implements OCRLibrary {
 
 	private final Logger logger = Logger.getLogger(TesseractLibrary.class.getName());
-	private final Tesseract tesseract;
+	private final ITesseract tesseract;
 
 	public TesseractLibrary() {
 		this.tesseract = new Tesseract();
