6a77167601529d116c6ac48fc49a1e5ac8cf15fc
Michiel Devriese <michieldevriese@gmail.com>
2017-07-25 15:56:53 +0200

    Fileconverters to convert result files in a format this project will handle

---
+++ src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterA.java
@@ -0,0 +1,46 @@
+package be.mrtus.ocrbenchmark.domain.fileconverters;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileConverterA extends Thread {
+
+	@Override
+	public void run() {
+		Path path = Paths.get("x:/Challenge1_Test_Task3_GT.txt");
+		Path output = Paths.get("x:/output");
+
+		try {
+			Files.readAllLines(path)
+					.forEach(l -> {
+						String[] split = l.split(", ");
+						String fileName = split[0];
+						fileName = fileName.substring(0, fileName.length() - 4);
+
+						String fileContents = split[1].replaceAll("\"", "");
+
+						Path newPath = output.resolve(fileName + ".txt");
+
+						try {
+							Files.createFile(newPath);
+						} catch(IOException ex) {
+							Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, ex);
+						}
+
+						try(BufferedWriter bw = new BufferedWriter(new FileWriter(newPath.toFile()))) {
+							bw.write(fileContents);
+						} catch(Exception e) {
+							Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
+						}
+					});
+		} catch(IOException e) {
+			Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
+		}
+	}
+}
---
+++ src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterB.java
@@ -0,0 +1,52 @@
+package be.mrtus.ocrbenchmark.domain.fileconverters;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileConverterB extends Thread {
+
+	@Override
+	public void run() {
+		Path path = Paths.get("x:/Challenge1_Training_Task1_GT");
+		Path output = Paths.get("x:/output2");
+
+		try {
+			Files.walk(path)
+					.filter(Files::isRegularFile)
+					.forEach(p -> {
+						Path filename = p.getFileName();
+						Path newPath = output.resolve(filename);
+
+						try {
+							Files.createFile(newPath);
+
+							BufferedWriter bw = new BufferedWriter(new FileWriter(newPath.toFile()));
+
+							StringBuilder fileContents = new StringBuilder();
+
+							Files.readAllLines(p)
+							.forEach(l -> {
+								String[] split = l.split(", ");
+								String value = split[4].replace("\"", "");
+
+								fileContents.append(value);
+								fileContents.append("\n");
+							});
+
+							bw.write(fileContents.toString());
+							bw.flush();
+						} catch(IOException ex) {
+							Logger.getLogger(FileConverterB.class.getName()).log(Level.SEVERE, null, ex);
+						}
+					});
+		} catch(IOException ex) {
+			Logger.getLogger(FileConverterB.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
