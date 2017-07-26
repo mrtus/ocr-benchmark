9b7c7e05042a6ed320745d43c3283faa1235d416
Michiel Devriese <michieldevriese@gmail.com>
2017-07-26 16:59:31 +0200

    Setter for target

--- src/main/java/be/mrtus/ocrbenchmark/domain/entities/ProcessResult.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/entities/ProcessResult.java
@@ -83,4 +83,8 @@ public class ProcessResult implements Serializable {
 	public void setResult(String result) {
 		this.result = result;
 	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
 }
