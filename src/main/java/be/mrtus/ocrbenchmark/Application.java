a4ca903fdc326aac7a64888a5d50bfa7135c3d58
Michiel Devriese <michieldevriese@gmail.com>
2017-07-26 17:21:15 +0200

    Use different benchmark modes

--- src/main/java/be/mrtus/ocrbenchmark/Application.java
+++ src/main/java/be/mrtus/ocrbenchmark/Application.java
@@ -1,18 +1,39 @@
 package be.mrtus.ocrbenchmark;
 
+import be.mrtus.ocrbenchmark.application.config.properties.BenchmarkConfig;
+import be.mrtus.ocrbenchmark.application.config.properties.FileConverterConfig;
 import be.mrtus.ocrbenchmark.domain.Benchmark;
+import be.mrtus.ocrbenchmark.domain.fileconverters.FileConverterFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
+	@Autowired
+	private Benchmark benchmark;
+	@Autowired
+	private BenchmarkConfig config;
+	@Autowired
+	private FileConverterFactory factory;
+	@Autowired
+	private FileConverterConfig fileConverterConfig;
+
+	public Application() throws InterruptedException {
+
+		if(this.config.getMode().equalsIgnoreCase("BENCHMARK")) {
+			this.benchmark.start();
+		} else if(this.config.getMode().equalsIgnoreCase("CONVERT")) {
+			Thread thread = this.factory.create(this.fileConverterConfig.getType());
+
+			thread.start();
+
+			thread.join();
+		}
+	}
+
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(Application.class, args);
-
-		Benchmark benchmark = context.getBean(Benchmark.class);
-
-		benchmark.start();
+		SpringApplication.run(Application.class, args);
 	}
 }
