a0025cf573586b4dc61e65c61c4b766012de251a
Michiel Devriese <michieldevriese@gmail.com>
2017-07-26 13:03:00 +0200

    Database storage instead of files

--- src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterA.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterA.java
@@ -1,43 +1,32 @@
 package be.mrtus.ocrbenchmark.domain.fileconverters;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import be.mrtus.ocrbenchmark.domain.entities.Annotation;
+import be.mrtus.ocrbenchmark.persistence.AnnotationRepository;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class FileConverterA extends Thread {
+
+	@Autowired
+	private AnnotationRepository annotationRepository;
 
 	@Override
 	public void run() {
 		Path path = Paths.get("x:/Challenge1_Test_Task3_GT.txt");
-		Path output = Paths.get("x:/output");
 
 		try {
 			Files.readAllLines(path)
 					.forEach(l -> {
 						String[] split = l.split(", ");
 						String filename = split[0];
-						filename = filename.substring(0, filename.length() - 4);
-
 						String fileContents = split[1].replaceAll("\"", "");
 
-						Path newFile = output.resolve(filename + ".txt");
-
-						try {
-							Files.createFile(newFile);
-						} catch(IOException ex) {
-							Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, ex);
-						}
-
-						try(BufferedWriter bw = new BufferedWriter(new FileWriter(newFile.toFile()))) {
-							bw.write(fileContents);
-						} catch(Exception e) {
-							Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
-						}
+						this.annotationRepository.save(new Annotation(filename, fileContents));
 					});
 		} catch(IOException e) {
 			Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
