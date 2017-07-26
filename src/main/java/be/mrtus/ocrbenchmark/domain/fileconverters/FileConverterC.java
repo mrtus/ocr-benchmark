69ef329aaeece3d6bbec358f5caab898c65f5a69
Michiel Devriese <michieldevriese@gmail.com>
2017-07-26 12:53:04 +0200

    Ignore file fails

--- src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterC.java
+++ src/main/java/be/mrtus/ocrbenchmark/domain/fileconverters/FileConverterC.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,35 +14,47 @@ public class FileConverterC extends Thread {
 
 	@Override
 	public void run() {
-		Path path = Paths.get("h:/images3");
-		Path output = Paths.get("h:/output3");
+		Path path = Paths.get("x:/images3");
+		Path output = Paths.get("x:/output3");
 
+		AtomicInteger count = new AtomicInteger();
+		
 		try {
 			Files.walk(path)
 					.filter(Files::isRegularFile)
 					.forEach(p -> {
-						String filename = p.getFileName().toString();
-						String parentPathName = p.getParent().toString();
-
-						parentPathName = parentPathName.replace("images3", "output3");
-
-						String[] split = filename.split("_");
-						String fileContents = split[1];
-
-						Path parentPath = Paths.get(parentPathName);
-
-						filename = filename.substring(0, filename.length() - 4);
-						Path newFile = parentPath.resolve(filename + ".txt");
-
 						try {
-							Files.createDirectories(parentPath);
-							Files.createFile(newFile);
-						} catch(IOException ex) {
-							Logger.getLogger(FileConverterC.class.getName()).log(Level.SEVERE, null, ex);
-						}
+							System.out.println("Processing file: " + p);
 
-						try(BufferedWriter bw = new BufferedWriter(new FileWriter(newFile.toFile()))) {
-							bw.write(fileContents);
+							String filename = p.getFileName().toString();
+							String parentPathName = p.getParent().toString();
+
+							parentPathName = parentPathName.replace("images3", "output3");
+
+							String[] split = filename.split("_");
+							String fileContents = split[1];
+
+							Path parentPath = Paths.get(parentPathName);
+
+							filename = filename.substring(0, filename.length() - 4);
+							Path newFile = parentPath.resolve(filename + ".txt");
+
+							try {
+								Files.createDirectories(parentPath);
+								Files.createFile(newFile);
+							} catch(IOException ex) {
+								Logger.getLogger(FileConverterC.class.getName()).log(Level.SEVERE, null, ex);
+							}
+
+							try(BufferedWriter bw = new BufferedWriter(new FileWriter(newFile.toFile()))) {
+								bw.write(fileContents);
+							} catch(Exception e) {
+								Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
+							}
+
+							count.addAndGet(1);
+							
+							System.out.print("Processed the " + count.get() + " file");
 						} catch(Exception e) {
 							Logger.getLogger(FileConverterA.class.getName()).log(Level.SEVERE, null, e);
 						}
