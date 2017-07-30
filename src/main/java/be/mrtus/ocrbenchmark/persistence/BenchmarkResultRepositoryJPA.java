fa2ccf13cc33d418c9ad1171999a6f8b6e4edc11
Michiel Devriese <michieldevriese@gmail.com>
2017-07-30 13:52:25 +0200

    Implement benchmark result repository findById

--- src/main/java/be/mrtus/ocrbenchmark/persistence/BenchmarkResultRepositoryJPA.java
+++ src/main/java/be/mrtus/ocrbenchmark/persistence/BenchmarkResultRepositoryJPA.java
@@ -3,6 +3,10 @@ package be.mrtus.ocrbenchmark.persistence;
 import be.mrtus.ocrbenchmark.domain.entities.BenchmarkResult;
 import java.util.UUID;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,7 +18,18 @@ public class BenchmarkResultRepositoryJPA implements BenchmarkResultRepository {
 	@Transactional
 	@Override
 	public BenchmarkResult findById(UUID id) {
-		return null;
+		CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+
+		CriteriaQuery<BenchmarkResult> criteria = builder.createQuery(BenchmarkResult.class);
+
+		Root<BenchmarkResult> root = criteria.from(BenchmarkResult.class);
+
+		Predicate where = builder.equal(root.get("id"), id);
+
+		criteria.where(where);
+
+		return this.entityManager.createQuery(criteria)
+				.getSingleResult();
 	}
 
 	@Transactional
